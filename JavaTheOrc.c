1cc6a2b22395182b68df060625f4804835b2ac77
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-03-17 02:33:15 +0000

    First commit

---
+++ JavaTheOrc.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  JavaTheOrc.c
+ *       	Usage:  ./JavaTheOrc.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/17/2017 02:32:16 AM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O*/
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
+
+
+//CAN YOU SEE THIS?
+
+
+
+/* Function Definitions */
+
+

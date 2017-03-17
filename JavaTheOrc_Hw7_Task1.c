34226a763a6b6d200c255c3cd0b1f6211fa31690
Daniel Tayler <danieltayler@mail.weber.edu>
2017-03-17 20:32:57 +0000

    got confused

--- JavaTheOrc_Hw7_Task1.c
+++ JavaTheOrc_Hw7_Task1.c
@@ -13,14 +13,27 @@
  */
 #include <stdio.h>		/* For Standard I/O*/
 #include <stdlib.h>
+#define LEN 5
 
 /* Function Prototypes */
+void copy_arr (double ar1[], const double ar2[], int n);
+void copy_ptr (double *ar1, const double *ar2, int n);
+void copy_ptrs (double *ar1, const double *start, const double * end);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
 
+	double source[LEN] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	double target1[LEN];
+	double target2[LEN];
+//	double target3[LEN];
 
+	// this is a test
+	copy_arr ( target1, source, LEN);
+	copy_ptr ( target2, source, LEN);
+	printf(" %lf\n", target1[4]);
+	printf(" %lf\n", target2[4]);
 	return 0;
 }
 
@@ -28,3 +41,27 @@ int main(int argc, char *argv[])
 /* Function Definitions */
 
 
+void copy_arr (double ar1[], const double ar2[], int n)
+{	
+	for (int i = 0; i < n; i++)
+	{
+		ar1[i] = ar2 [i];
+	}
+	return;
+}
+
+void copy_ptr (double *ar1, const double *ar2, int n)
+{	
+	for (int i = 0; i < n; i++)
+	{
+		ar1[i] = ar2 [i];
+	}
+	return;
+}
+
+
+void copy_ptrs (double *ar1, const double *start, const double * end)
+{
+	return;
+
+}

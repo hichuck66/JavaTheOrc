d05f3d7662a6a9cafd231876f278e20329c94d88
ChikereNjoku Chibuike <cchikerenjoku@weber.edu>
2017-03-17 13:51:08 +0000

    Task1 FIRST commit

---
+++ JavaTheOrc_Hw7_Task1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  JavaTheOrc_Hw7_Task1.c
+ *       	Usage:  ./JavaTheOrc_Hw7_Task1.c
+ *    Description:  TASK 1
+ *
+ *        Version:  1.0
+ *        Created:  03/17/2017 01:49:29 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chikere-Njoku Chibuike (), cchikerenjoku@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O*/
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+

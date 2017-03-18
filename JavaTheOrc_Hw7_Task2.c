de15bbd8d87b23cbf9cc7fc20a47bb6573782929
Daniel Tayler <danieltayler@mail.weber.edu>
2017-03-18 21:49:01 +0000

    i htink we got this bad boy

--- JavaTheOrc_Hw7_Task2.c
+++ JavaTheOrc_Hw7_Task2.c
@@ -18,7 +18,6 @@
 
 #define SIZE 10
  
-int max, min,randd[SIZE];
 
 /* Function Prototypes */
 void MaxMin(int num, int vals[], int * min, int * max);
@@ -26,6 +25,8 @@ void MaxMin(int num, int vals[], int * min, int * max);
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	int max, min,randd[SIZE];
+	 //randd  = { 0,0,0,0,0,0,0,0,0,0};
 	MaxMin ( SIZE, randd, &min, &max);
 
 	return 0;
@@ -36,30 +37,31 @@ int main(int argc, char *argv[])
 
 void MaxMin(int num, int vals[], int * min, int * max)
 {	
-	int vals[10];
+	//int vals[10];
 	//assume the first element as maximu and minimum
 	//Hugo made mention of something like this using a tunnel reference
-	min = vals[0];
-	max = vals[0];
 	//use the loop to find the max and min in the array
+	
+	*min = vals[0];
+	*max = vals[0];
+	printf("The list is: \n");
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < num; i ++)
 	{
-		num = 0;  
+	//	num = 0;  
 		vals[i] = rand();
-		printf("The list is: \n");
 		printf(" %d\n ", vals[i]);
 		if (vals[i] > *max)
 		{
-			max = vals[i];
+			*max = vals[i];
 		}
 		else if (vals[i] < *min)
 		{
-			min = vals[i];
+			*min = vals[i];
 		}
 	}
-	printf("The maximum value is %d\n", max);
-	printf("The minimum value is %d\n", min);
+	printf("The maximum value is %d\n", *max);
+	printf("The minimum value is %d\n", *min);
 
 	return;
 }

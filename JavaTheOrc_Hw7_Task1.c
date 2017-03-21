e97c8663eaf1146cc867db28ac55bdd568742ab3
Daniel Tayler <danieltayler@mail.weber.edu>
2017-03-21 16:55:47 +0000

    we almost got it just have to check what hte hell the bloody n is supposed to be and do

--- JavaTheOrc_Hw7_Task1.c
+++ JavaTheOrc_Hw7_Task1.c
@@ -19,6 +19,7 @@
 void copy_arr (double ar1[], const double ar2[], int n);
 void copy_ptr (double *ar1, const double *ar2, int n);
 void copy_ptrs (double *ar1, const double *start, const double * end);
+void Show ( double ar []);
 
 /* Main Program */
 int main(int argc, char *argv[])
@@ -31,9 +32,17 @@ int main(int argc, char *argv[])
 //	double target3[LEN];
 
 	// this is a test
+	printf("source arry: ");
+	Show ( source);
 	copy_arr ( target1, source, LEN);
+	printf("copied by subscript: ");
+	Show(target1);
 	copy_ptr ( target2, source, LEN);
-	copy_ptrs ( target3, source, source[LEN]);
+	printf("copied by pointer: ");
+	Show(target2);
+	copy_ptrs ( target3, source, &source[LEN]);
+	printf("copied using pointer range: ");
+	Show(target3);
 	return 0;
 }
 
@@ -56,7 +65,7 @@ void copy_ptr (double *ar1, const double *ar2, int n)
 	for (int i = 0; i < LEN; i++)
 	{
 		*(ar1) = *(ar2);
-		printf("source array:[%lf]\n", *ar1++);
+		printf("source 1111array:[%lf]\n", *ar1);
 		ar1++;
 		ar2++;
 	}
@@ -66,13 +75,28 @@ void copy_ptr (double *ar1, const double *ar2, int n)
 
 void copy_ptrs (double *ar1, const double *start, const double * end)
 {
-	while (*start < *end)
+	while (*start != *end)
 	{
 		*(ar1) = *(start);
-		printf("source array:[%lf]\n", *start++);
+		printf("source 222222array:[%lf]\n", *ar1);
 		start++;
+		ar1++;
 
 	}
 	return;
-
 }
+
+
+
+void Show ( double ar [])
+{
+	for (int i = 0; i < LEN; i++)
+	{
+		printf( "%.2lf " , *ar);
+		ar++;
+	}
+	printf("\n");
+	return;
+}
+
+
